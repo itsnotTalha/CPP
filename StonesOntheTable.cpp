9ad73dc21af83c0d3f063b000bf1da7010791122
itsnotTalha <talhajubayer737@gmail.com>
2025-01-30 20:37:44 +0600

    Codeforces - Stones on the table

--- StonesOntheTable.cpp
+++ StonesOntheTable.cpp
@@ -1,18 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n, count=0,maxC=0;
+
+int main() {
+    int n, count = 0;
     string s;
-    cin >> n;
-    cin >> s;
-    for(int i=0; i<n-1; i++){
-        if(s[i]==s[i+1]){
-            count++;
-           maxC = max(maxC,count);
-        }else{
-            count = 0;
+    
+    cin >> n >> s;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1]) { 
+            count++;  // Increment count if adjacent stones are the same6
         }
     }
-    cout<<maxC;
+
+    cout << count;
     return 0;
 }
