3523b3431bff39a598730a16a0a9b48672f9522b
itsnotTalha <talhajubayer737@gmail.com>
2025-01-30 20:24:18 +0600

    Codeforces - Bear and Big Brother

---
+++ StonesOntheTable.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, count=0,maxC=0;
+    string s;
+    char str=s[0];
+    cin >> n;
+    cin >> s;
+    for(int i=0; i<n-1; i++){
+        if(s[i]==s[i+1]){
+            count++;
+           maxC = max(maxC,count);
+        }else{
+            count = 0;
+        }
+    }
+    cout<<maxC;
+    return 0;
+}
