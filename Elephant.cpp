b8bc2dd25927c86b312f9096f9f4c5a1433db3c3
itsnotTalha <talhajubayer737@gmail.com>
2025-01-31 09:02:49 +0600

    Codeforces - Elephant

--- Elephant.cpp
+++ Elephant.cpp
@@ -1,18 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int  main(){
-    int x;
+    long long int x;
     cin >> x;
-    if(x%5==0){
-        cout << x/5;
-    }else if(x%4==0){
-        cout << x / 4;
-    }else if(x%3==0){
-        cout << x/4;
-    }else if(x%2==0){
-        cout<< x/2;
-    }else{
-        cout << x/1;
-    }
+    cout << (x + 4) / 5;
     return 0;
 }
