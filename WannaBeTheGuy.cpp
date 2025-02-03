6ae117453f16cdfb18bec1400478c42ab868de07
itsnotTalha <talhajubayer737@gmail.com>
2025-02-03 17:41:18 +0600

    Codeforces - Arrival of General

---
+++ WannaBeTheGuy.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    bool a=false;
+    set<int>result;
+    cin>>n;
+
+    int p;
+    cin>>p;
+    for(int i=0; i<p;i++){
+        int tmp;
+        cin>>tmp;
+        result.insert(tmp);
+    }
+    int q;
+    cin>>q;
+    for(int i=0; i<q;i++){
+        int tmp;
+        cin>>tmp;
+        result.insert(tmp);
+    }
+    
+     if (result.size() == n) {
+        cout << "I become the guy.";
+    } else {
+        cout << "Oh, my keyboard!";
+    }
+
+    return 0;
+}
