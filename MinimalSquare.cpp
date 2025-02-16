5c0a30337dc3f18cf4476f9af1d569d872ad89b2
itsnotTalha <talhajubayer737@gmail.com>
2025-02-16 15:05:15 +0600

    Codeforces -  Minimal square

---
+++ MinimalSquare.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        int side = max(max(a, b), 2 * min(a, b));
+        cout << side * side << endl;
+    }
+    return 0;
+}
