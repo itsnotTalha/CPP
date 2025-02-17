cfcaca9280b1634a93423e4dc5059ff59e065c7c
itsnotTalha <talhajubayer737@gmail.com>
2025-02-17 07:22:58 +0600

    Codeforces -  A+B?

---
+++ A+B.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int s[2];
+        cin>>s[0]>>s[1];
+        cout<<s[0]+s[1]<<endl;
+    }
+    return 0;
+}
