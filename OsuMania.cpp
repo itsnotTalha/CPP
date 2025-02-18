e8a4cf616a23a04af4c76010a8ff05d842b70fbd
itsnotTalha <talhajubayer737@gmail.com>
2025-02-18 21:45:27 +0600

    Codeforces - osu!mania

---
+++ OsuMania.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s[n];
+        for(int i=0;i<n;i++){
+           cin>>s[i];
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<4;j++){
+                if(s[i][j]=='#'){
+                    cout<<j+1<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
