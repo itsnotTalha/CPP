d6f772f4e793960b69a59eb50d86f2c767b32872
itsnotTalha <talhajubayer737@gmail.com>
2025-02-20 08:58:09 +0600

    Weekly Long Contest - 17 | UIUCPC F Time limit exceeded

---
+++ F-F.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N), C(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> C[i];
+    }
+
+    unordered_map<int, int> freq;
+    for (int j = 0; j < N; j++) {
+        freq[B[C[j] - 1]]++;  
+    }
+
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        count += freq[A[i]];  
+    }
+
+    cout << count << endl;
+    return 0;
+}
