4a553814391c40c24917cfc13398c6168f771a37
itsnotTalha <talhajubayer737@gmail.com>
2025-02-21 19:24:35 +0600

    Weekly Long Contest - 17 | UIUCPC E

--- E-D.cpp
+++ E-D.cpp
@@ -2,55 +2,57 @@
 using namespace std;
 
 void solve() {
-    string s;
+    char s[100005];  // Use character array for speed
     int q;
-    cin >> s >> q;
-    
-    int n = s.size();
-    vector<int> flip(n + 1, 0); 
+    scanf("%s", s);
+    scanf("%d", &q);
 
-    vector<char> ans; 
+    int n = strlen(s);
+    vector<int> flip(n + 1, 0);  // Difference array for range updates
+
+    vector<int> queryIndices;  // Store query indices for fast access
+    queryIndices.reserve(q);
 
     for (int a = 0; a < q; a++) {
-        char tmp;
-        cin >> tmp;
-        if (tmp == 'I') {
+        char op;
+        scanf(" %c", &op);  // Read operation type ('I' or 'Q')
+
+        if (op == 'I') {
             int i, j;
-            cin >> i >> j;
-            i--; j--; 
-            flip[i] ^= 1; 
-            flip[j + 1] ^= 1;
-        } else {
+            scanf("%d %d", &i, &j);
+            i--; j--;  // Convert to 0-based index
+            flip[i] ^= 1;
+            if (j + 1 < n) flip[j + 1] ^= 1;
+        } else {  // 'Q' query
             int i;
-            cin >> i;
-            i--; 
-            
-            int flip_count = 0;
-            for (int k = 0; k <= i; k++) {
-                flip_count ^= flip[k]; 
-            }
-            
-            if (flip_count % 2 == 1) {
-                ans.push_back((s[i] == '0') ? '1' : '0');
-            } else {
-                ans.push_back(s[i]);
-            }
+            scanf("%d", &i);
+            i--;
+            queryIndices.push_back(i);
         }
     }
 
+    // Compute prefix XOR
+    for (int i = 1; i < n; i++) {
+        flip[i] ^= flip[i - 1];
+    }
+
+    // Output results efficiently
     static int case_num = 1;
-    cout << "Case " << case_num++ << ": " << endl;
-    for (char x : ans) {
-        cout << x << endl;
+    printf("Case %d:\n", case_num++);
+    for (int i : queryIndices) {
+        if (flip[i] % 2 == 1) {
+            printf("%c\n", s[i] == '0' ? '1' : '0');
+        } else {
+            printf("%c\n", s[i]);
+        }
     }
 }
 
 int main() {
     int T;
-    cin >> T;
+    scanf("%d", &T);
     while (T--) {
         solve();
     }
-
     return 0;
 }
