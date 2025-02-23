d40552f60ca3effcdfe7ca6700a46273a7125da2
itsnotTalha <talhajubayer737@gmail.com>
2025-02-23 07:46:38 +0600

    Codeforces - Easy Problem

---
+++ EasyProblem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define si(x) scanf("%lld",&x)
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        si(n);
+        cout<<n-1<<endl;
+    }
+    return 0;
+}
