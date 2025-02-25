b3e8f6c27b520057c0199d62fd419560ad351ad3
itsnotTalha <talhajubayer737@gmail.com>
2025-02-25 19:36:18 +0600

    Codeforces - IQ Test

---
+++ IQTest.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int oc=0,ec=0;
+    int c1=0,c2=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(a[i]%2==0){
+            c1++;
+            ec = a[i]; 
+        }else{
+            c2++;
+            oc = a[i]; 
+        }
+    }
+    if(c1==1){
+        cout<<ec;
+    }else{
+        cout<<oc;
+    }
+    return 0;
+}
