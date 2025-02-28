45a530d54101c8100969f2f9f4cda2fdac14d4a1
itsnotTalha <talhajubayer737@gmail.com>
2025-02-28 17:17:16 +0600

    Weekly Long Contest - 18 | UIUCPC G - Capitalized

---
+++ G-Capitalized.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) 
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end() 
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(a) int((a).size())
+
+using namespace std;
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string S;
+    cin>>S;
+    if (isupper(S[0]) && all_of(S.begin() + 1, S.end(), ::islower)) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+
+    return 0;
+}
