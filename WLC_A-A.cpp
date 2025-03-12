3f1f6aca506ec07b2e173c0f7d96b223bdc2de11
itsnotTalha <talhajubayer737@gmail.com>
2025-03-13 00:01:10 +0600

    Weekly Long Contest - 20 - A - A

--- WLC_A-A.cpp
+++ WLC_A-A.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
     long long int n,m,a;
     cin>>n>>m>>a;
-    long long int cnt=0;
-    (n%a==0?cnt+=n/a:cnt+=(n/a)+1);
-    (m%a==0?cnt+=m/a:cnt+=(m/a)+1);
-    cout<<cnt;
+     
+    long long w = (n + a - 1) / a;
+    long long h = (m + a - 1) / a;
+    
+    cout <<w*h<<endl;
     return 0;
 }
