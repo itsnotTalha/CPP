43a2792da9942050d23a2b0399a3baa1219684e7
itsnotTalha <talhajubayer737@gmail.com>
2025-03-17 16:03:33 +0600

    UIU BCPC-251 1st Individual - A

---
+++ A-AreaOfcircle.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+const double P = 3.141592653;
+int main(){
+    double R;
+    cin>>R;
+    cout << fixed << setprecision(9);
+    cout<<R*R*P<<endl;
+}
