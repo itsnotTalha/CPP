662425d798be43e0b18e9e15c07b10206f792c14
itsnotTalha <talhajubayer737@gmail.com>
2025-03-21 16:04:55 +0600

    Ramadan Code Blitz 2025 - B

---
+++ RamdanCodeBlitz2025/B.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int A,B;
+        cin>>A>>B;
+        if((A+B)%2==0) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+}
