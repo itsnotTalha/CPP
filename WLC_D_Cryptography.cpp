e787101f3ba9bfc877ce36d1c98aba41c09c45b6
itsnotTalha <talhajubayer737@gmail.com>
2025-03-27 15:12:05 +0600

    Weekly long contest | UIUCPC - D - Cryptography

--- WLC_D_Cryptography.cpp
+++ WLC_D_Cryptography.cpp
@@ -4,38 +4,34 @@ using namespace std;
 #define for1(i,x) for(int i=1;i<=x;i++)
 typedef long long ll;
 
+const int maxLimit=200000;
+vector<bool>prime(maxLimit+1,true);
 
-bool isPrime(int num, const vector<int>& primes) {
-    for (int prime : primes) {
-        if (num % prime == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-int searchP(int n) {
-    if(n==1)return 2;
-    vector<int>pms={2};
-    int num = 3;
-    while (pms.size() < n) {
-        if (isPrime(num, pms)) {
-            pms.push_back(num);
-        }
-        num+=2;
-    }
-
-    return pms[n-1];
-}
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
-
+    prime[0]=prime[1]=false;
+        for(int i=2;i*i<=maxLimit;i++){
+            if(prime[i])
+            for(int j=i*i;j<=maxLimit;j+=i){
+                prime[j]=false;
+            }
+        }
+        vector<ll>ans;
+        //int i=2;
+        for (int i = 2; i <= maxLimit; i++) {
+            if (prime[i]) {
+                ans.push_back(i);  
+            }
+        }
     int k;
     cin>>k;
     while(k--){
-        int n;
+        int n,i=2,x=2;
         cin>>n;
-        cout<<searchP(n)<<endl;
+        int cnt=0;
+        
+        cout<<ans[n-1]<<endl;
+        //cout<<searchP(n)<<endl;
     }
 }
