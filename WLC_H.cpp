8b6c16628ab4e5c0e6f47f396ca812a6c065e72d
itsnotTalha <talhajubayer737@gmail.com>
2025-03-28 11:39:42 +0600

    Hoitese na

--- WLC_H.cpp
+++ WLC_H.cpp
@@ -6,24 +6,19 @@ int main(){
     int N;
     cin>>N;
     vector<int>a(N);
-    for0(i,N){
-        cin>>a[i];
-    }
-    int cnt = 1; 
-    int tmp = a[0];
-    int flag = 0; 
-    
-    for1(i,N-1) {
-        if (a[i] == tmp) {
-            cnt++; 
-            if (cnt == 3) {
-                flag = 1; 
-                break;    
-            } 
+    for0(i,N) cin>>a[i];
+    int cnt = 1, tmp = a[0], flag = 0;
+    for1(i,N-1){
+        if(a[i] == tmp){
+            cnt++;
+            if(cnt == 3){
+                flag = 1;
+                break;
+            }
         } else {
             tmp = a[i];
             cnt = 1;
         }
     }
-    cout<<(flag?"YES":"NO")<<endl;
+    cout<<(flag ? "YES" : "NO")<<endl;
 }
