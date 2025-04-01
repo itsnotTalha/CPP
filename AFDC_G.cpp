a51d293472e5ab0931e7dcd3fd2f7080cf6ad38d
itsnotTalha <talhajubayer737@gmail.com>
2025-04-01 21:56:15 +0600

    April Fools Day Contest 2025 - G - Wromg ans on test 3

--- AFDC_G.cpp
+++ AFDC_G.cpp
@@ -1,22 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 const double PI = 3.141592653589793;
 
-int main(){
+int main() {
     cout << fixed << setprecision(15);
+    
     int n, k;
-    cin>>n>>k;
+    cin >> n >> k;
     vector<double> a(n);
-    for(int i=0;i<n;i++){
+    
+    for (int i = 0; i < n; i++) {
         int x, y;
-        cin>>x>>y;
-        double d = sqrt(pow(x,2)+pow(y,2));
-        a[i]=d;
+        cin >> x >> y;
+        double d = sqrt(pow(x, 2) + pow(y, 2));
+        a[i] = d;
     }
+    
     sort(a.begin(), a.end());
+    
     int m = 0;
-    while((PI*m*m)<a[k-1]){
+    while (PI * m * m < a[k - 1]) { 
         m++;
     }
-    cout<<(PI*m*m)<<endl;
+    
+    cout << (PI * m * m) << endl;
+    
+    return 0;
 }
