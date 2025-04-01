c6a8fe46616cea2b70df8f8fafe0df163ec91084
itsnotTalha <talhajubayer737@gmail.com>
2025-04-01 22:02:01 +0600

    April Fools Day Contest 2025 - H

---
+++ AFDC_H.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x; cin>>x;
+    switch(x){
+        case 1 : cout<<"CODEFORCES";
+        break;
+        case 2 : cout<<"EYE";
+        break;
+        case 3 : cout<<"TESTING";
+        break;
+        case 4 : cout<<"SYSTEM";
+        break;
+        case 5 : cout<<"APRIL";
+        break;
+        case 6 : cout<<"FOOLS";
+        break;
+        case 7 : cout<<"DAY";
+        break;
+    }
+}
