723a7569b992fabe1f622ab7089f8cef4b056538
itsnotTalha <talhajubayer737@gmail.com>
2025-04-08 00:36:47 +0600

    Confused on addAt

--- DSA/DoublyLinkedList.cpp
+++ DSA/DoublyLinkedList.cpp
@@ -39,6 +39,30 @@ void addLast(int n){
     
 }
 
+void addAt(int n, int k){
+    Node* newNode = new Node;
+    newNode->value = n;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    Node* ptr = head;
+    int i = 1;
+    while(ptr->next!=NULL){
+        if(k<=0){
+            addFirst(n);
+            return;
+        }
+        if(i==k-1) break;
+        ptr = ptr->next;
+        i++;
+    }
+    Node* tmp = ptr->next;
+    newNode->next = ptr->next;
+    ptr->next = newNode;
+    tmp->prev = newNode;
+    newNode->prev = ptr;
+}
+
 void display(){
     Node* ptr = head;
     while(ptr!=NULL){
@@ -51,6 +75,7 @@ int main(){
     addFirst(10);
     addFirst(40);
     addFirst(30);
-    //addLast(55);
+    addLast(55);
+    addAt(20,2);
     display();
 }
