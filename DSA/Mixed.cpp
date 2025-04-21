c0a30649bdfb2c8bc4a7ef0d6ece2fc2be7e2ff2
itsnotTalha <talhajubayer737@gmail.com>
2025-04-21 22:43:15 +0600

    Jhalmuri selection sort

--- DSA/Mixed.cpp
+++ DSA/Mixed.cpp
@@ -39,8 +39,20 @@ void display(vector<int>A){
     cout<<endl;
 }
 
+void selectionSort(vector<int>&a){
+    int size = a.size();
+    for(int i = 0; i<size; i++){
+        int tmp = i;
+        for(int j = i+1; j<size; j++){
+            if(a[j]<a[tmp]) tmp = j;
+        }
+        swap(a[i], a[tmp]);
+    }
+}
+
 int main(){
     vector<int> a = {1, 5,3,2,6,7};
-    MSort(a, 0, 6);
+    //MSort(a, 0, 6);
+    selectionSort(a);
     display(a);
 }
