4e8bd7031ad865882bf73a2a9abc9f479cf3abb7
itsnotTalha <talhajubayer737@gmail.com>
2025-04-22 00:40:04 +0600

    Jhalmuri Linked List

--- DSA/Mixed.cpp
+++ DSA/Mixed.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node{
+    int value;
+    Node *next;
+};
+
+Node *Head = NULL;
+Node *Tail = NULL;
+
 void Merge(vector<int>&a, int l, int mid, int h){
     int size = (h-l+1);
     vector<int> tmp(size);
@@ -50,9 +58,75 @@ void selectionSort(vector<int>&a){
     }
 }
 
+void addFirst(int n){
+    Node *newNode = new Node;
+    newNode->value = n;
+    newNode->next = NULL;
+    if(Head==NULL){
+        Head = newNode;
+        Tail = newNode;
+    }else{
+        newNode->next = Head;
+        Head = newNode;
+    }
+    
+}
+
+void displayll(){
+    Node *ptr = Head;
+    while(ptr!=NULL){
+        cout<<ptr->value<<" ";
+        ptr = ptr->next;
+    }
+}
+
+void addLast(int n){
+    Node *newNode = new Node;
+    newNode->value = n;
+    newNode->next = NULL;
+    if(Head==NULL){
+        Head = newNode;
+        Tail = newNode;
+    }else{
+        Tail->next = newNode;
+        Tail = newNode;
+    }
+
+}
+
+vector<int> getVal(){
+    Node *ptr = Head;
+    vector<int> a;
+    while(ptr!=NULL){
+        a.push_back(ptr->value);
+        ptr = ptr->next;
+    }
+    return a;
+}
+
+void sortll(){
+    vector<int> a = getVal();
+    MSort(a, 0, a.size());
+    Node *ptr = Head;
+    int i = 0;
+    while(ptr!=NULL){
+        ptr->value = a[i++];
+        ptr = ptr->next;
+    }
+}
+
 int main(){
     vector<int> a = {1, 5,3,2,6,7};
     //MSort(a, 0, 6);
     selectionSort(a);
     display(a);
+
+    addFirst(10);
+    addFirst(50);
+    addFirst(30);
+    addLast(50);
+    addLast(80);
+    addLast(70);
+    sortll();
+    displayll();
 }
