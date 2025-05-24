cd931bd6e8a1266b2398d043f0b3c11491603995
itsnotTalha <talhajubayer737@gmail.com>
2025-05-24 07:08:04 +0600

    Graph direction check

---
+++ DSA/GraphMatrixDirection.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define sz(x) int(x.size())
+
+#define for0(i, n) for (int i = 0; i < int(n); i++)
+#define for1(i, n) for (int i = 1; i <= int(n); i++)
+#define FOR(i, x, y) for (int i = int(x); i <= int(y); i++)
+#define ROF(i, x, y) for (int i = int(x); i >= int(y); i--)
+
+#define si(x) scanf("%d", &x)
+#define sii(x, y) scanf("%d %d", &x, &y)
+#define siii(x, y, z) scanf("%d %d %d", &x, &y, &z)
+#define sl(x) scanf("%lld", &x)
+#define sll(x, y) scanf("%lld %lld", &x, &y)
+#define slll(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
+
+#define ip(a, n) for0(i, n) cin >> a[i]
+
+int G[10][10],V,Q=0;
+
+void solve(){
+    for1(i,V){
+        for1(j,V){
+            cin>>G[i][j];
+        }
+    }
+    for1(i,V){
+        for1(j,V){
+            if(G[i][j]!=G[j][i]){
+                cout<<"Directed Graph"<<endl;
+                return;
+            }
+        }
+    }
+    cout<<"UnDirected Graph"<<endl;
+    return;
+}
+
+int main() {
+    Fast_io;
+    cin>>V;
+    solve();
+    return 0;
+}
