49f75411e0048ae3653dac06b6b0ed0b7f980a6b
itsnotTalha <talhajubayer737@gmail.com>
2025-05-26 22:37:30 +0600

    Codeforces - DIV 3 - E

--- E_Kirei_Attacks_the_Estate.cpp
+++ E_Kirei_Attacks_the_Estate.cpp
@@ -21,56 +21,59 @@ typedef pair<int, int> pii;
 #define FOR(i, x, y) for (int i = int(x); i <= int(y); i++)
 #define ROF(i, x, y) for (int i = int(x); i >= int(y); i--)
 
-#define si(x) scanf("%d", &x)
-#define sii(x, y) scanf("%d %d", &x, &y)
-#define siii(x, y, z) scanf("%d %d %d", &x, &y, &z)
-#define sl(x) scanf("%lld", &x)
-#define sll(x, y) scanf("%lld %lld", &x, &y)
-#define slll(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
-
-#define ip(a, n) for0(i, n) cin >> a[i]
-
-struct St{
+struct St {
     int u, p, d;
     ll pref, mn, mx;
 };
 
-vector<ll> a;
-vector<vi> adj;
-vector<ll> threat;
-
-void dfs(int u, int p, ll sum, ll min_sum, ll max_sum, int depth) {
-    ll current_sum = sum + ((depth % 2 == 0) ? a[u] : -a[u]);
-    threat[u] = max({threat[u], current_sum - min_sum, max_sum - current_sum});
-    min_sum = min(min_sum, current_sum);
-    max_sum = max(max_sum, current_sum);
-    for (int v : adj[u]) {
-        if (v != p) {
-            dfs(v, u, current_sum, min_sum, max_sum, depth + 1);
-        }
-    }
-}
-
 int main() {
     Fast_io;
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--) {
-        int n; cin >> n;
-        a.resize(n + 1);
-        adj.assign(n + 1, vi());
-        threat.assign(n + 1, LLONG_MIN);
+        int n;
+        cin >> n;
+        vector<ll> a(n + 1);
         for1(i, n) {
             cin >> a[i];
         }
-        for0(i, n - 1) {
-            int u, v; cin >> u >> v;
-            adj[u].pb(v);
-            adj[v].pb(u);
+
+        vector<vi> h(n + 1);
+        int u, v;
+        for (int i = 1; i < n; i++) {
+            cin >> u >> v;
+            h[u].pb(v);
+            h[v].pb(u);
         }
-        dfs(1, 0, 0, 0, 0, 0);
+
+        vector<ll> thrt(n + 1);
+        vector<St> st;
+        st.pb({1, 0, 0, a[1], 0, 0});
+
+        while (!st.empty()) {
+            St crnt = st.back();
+            st.pop_back();
+            if (crnt.d % 2 == 0) {
+                thrt[crnt.u] = crnt.pref - crnt.mn;
+            } else {
+                thrt[crnt.u] = crnt.mx - crnt.pref;
+            }
+
+            ll nmin = min(crnt.pref, crnt.mn);
+            ll nmax = max(crnt.pref, crnt.mx);
+
+            for (int x : h[crnt.u]) {
+                if (x != crnt.p) {
+                    int nDepth = crnt.d + 1;
+                    ll cPref = crnt.pref + ((nDepth % 2 == 0) ? a[x] : -a[x]);
+                    st.pb({x, crnt.u, nDepth, cPref, nmin, nmax});
+                }
+            }
+        }
+
         for1(i, n) {
-            cout << threat[i] << (i != n ? ' ' : '\n');
+            cout << thrt[i] << (i != n ? ' ' : '\n');
         }
     }
     return 0;
-}   
+}
