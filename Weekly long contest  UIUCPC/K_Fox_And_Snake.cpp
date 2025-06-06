c1a930c4e40b282723f54260947d1539ec277802
itsnotTalha <talhajubayer737@gmail.com>
2025-06-06 19:31:19 +0600

    Weekly Long Contest (29+30) - K

---
+++ Weekly long contest  UIUCPC/K_Fox_And_Snake.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define sz(x) int(x.size())
+
+#define for0(i, n) for (int i = 0; i < int(n); i++)
+#define for1(i, n) for (int i = 1; i <= int(n); i++)
+#define FOR(i, x, y) for (int i = int(x); i <= int(y); i++)
+#define ROF(i, x, y) for (int i = int(x); i >= int(y); i--)
+
+#define si(x) scanf("%d", &x)
+#define sii(x, y) scanf("%d %d", &x, &y)
+#define siii(x, y, z) scanf("%d %d %d", &x, &y, &z)
+#define sl(x) scanf("%lld", &x)
+#define sll(x, y) scanf("%lld %lld", &x, &y)
+#define slll(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
+
+#define ip(a, n) for0(i, n) cin >> a[i]
+
+int main() {
+    Fast_io;
+int n,m,tmp=0;
+    cin>>n>>m;
+    for(int i=0; i<n;i++){
+        if(i%2==0){
+            for(int j=0; j<m;j++){
+                cout<<"#";
+            }
+        }else if(!tmp){
+            for(int j=0;j<m;j++){
+                cout<<(j==m-1?"#":".");
+            }
+            tmp++;
+        }else if(tmp){
+            for(int j=0;j<m;j++){
+                cout<<(j==0?"#":".");
+            }
+            tmp--;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
