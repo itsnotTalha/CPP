394000a6741010311e720d413dec171325865c1b
itsnotTalha <talhajubayer737@gmail.com>
2025-06-06 06:53:30 +0600

    Weekly Long Contest (29+30) - C - Hoyna

---
+++ Weekly long contest  UIUCPC/C_Clock_Hands.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define sz(x) int(x.size())
+
+#define for0(i, n) for (int i = 0; i < int(n); i++)
+#define for1(i, n) for (int i = 1; i <= int(n); i++)
+#define FOR(i, x, y) for (int i = int(x); i <= int(y); i++)
+#define ROF(i, x, y) for (int i = int(x); i >= int(y); i--)
+
+#define si(x) scanf("%d", &x)
+#define sii(x, y) scanf("%d %d", &x, &y)
+#define siii(x, y, z) scanf("%d %d %d", &x, &y, &z)
+#define sl(x) scanf("%lld", &x)
+#define sll(x, y) scanf("%lld %lld", &x, &y)
+#define slll(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
+
+#define ip(a, n) for0(i, n) cin >> a[i]
+
+int main() {
+    Fast_io;
+    string s; cin>>s;
+    string s1,s2;
+    if(s.size()==5){
+        s1+=s[0];
+        s1+=s[1];
+        s2+=s[3];
+        s2+=s[4];
+    }else{
+        s1+=s[0];
+        s2+=s[2];
+        s2+=s[3];
+    }
+    int h = stoi(s1);
+    int m = stoi(s2);
+    double ans = abs(30*h - ((double)11/2)*(m));
+    ans = min(ans, 360-ans);
+    cout << fixed << setprecision(3) << ans << endl;
+    return 0;
+}
