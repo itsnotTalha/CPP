f39f551b862079dc1759ba5ea06dd87c93ab4626
itsnotTalha <talhajubayer737@gmail.com>
2025-06-19 22:10:39 +0600

    Weekly long contest 31 - A

---
+++ Weekly long contest  UIUCPC/A_Triple.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define sz(x) int(x.size())
+
+#define for0(i, n) for (int i = 0; i < int(n); i++)
+#define for1(i, n) for (int i = 1; i <= int(n); i++)
+#define FOR(i, x, y) for (int i = int(x); i <= int(y); i++)
+#define ROF(i, x, y) for (int i = int(x); i >= int(y); i--)
+
+#define si(x) scanf("%d", &x)
+#define sii(x, y) scanf("%d %d", &x, &y)
+#define siii(x, y, z) scanf("%d %d %d", &x, &y, &z)
+#define sl(x) scanf("%lld", &x)
+#define sll(x, y) scanf("%lld %lld", &x, &y)
+#define slll(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
+
+#define ip(a, n) for0(i, n) cin >> a[i]
+
+void solve(){
+    int n; cin>>n;
+        vi a(n);
+        ip(a,n);
+        vi freq(n+10,0);
+        for0(i,n){
+            freq[a[i]]++;
+            if(freq[a[i]]>=3){
+                cout<<a[i]<<endl;
+                return;
+            }
+        }
+        cout<<-1<<endl;
+}
+
+int main() {
+    Fast_io;
+    int t; cin>>t; 
+    while(t--){
+        solve();
+    }
+    return 0;
+}
