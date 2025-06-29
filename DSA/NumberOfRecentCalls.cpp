8aa3a095d95c0e590a2713475fd479d031c05b13
itsnotTalha <talhajubayer737@gmail.com>
2025-06-29 06:30:22 +0600

    DSA lab assignment

---
+++ DSA/NumberOfRecentCalls.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>record;
+int start; 
+void RecentCounter() {start = 0;} 
+int main(){
+    
+}
---
+++ DSA/QueeuUsingStack.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+    stack<int> q;
+    void push(int x) {
+        
+    }
+    
+    int pop() {
+        
+    }
+    
+    int peek() {
+        
+    }
+    
+    bool empty() {
+        
+    }
+int main(){
+
+}
