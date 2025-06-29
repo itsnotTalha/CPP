8cb2c2fbb7715f13e9d371e636bf47d000e4233d
itsnotTalha <talhajubayer737@gmail.com>
2025-06-29 21:43:19 +0600

    DSA lab assignment Tree

---
+++ DSA/DSA_Assignment/FindModeinBST.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* prevNode = nullptr; 
+    int currCount = 0;            
+    int maxCount = 0;              
+    vector<int> modes;             
+
+    void inorder(TreeNode* node) {
+        if (node == NULL) return;
+
+        inorder(node->left);
+
+        if (prevNode != NULL && prevNode->val == node->val) {
+            currCount++; 
+        } else {
+            currCount = 1; 
+        }
+
+        if (currCount > maxCount) {
+            maxCount = currCount;
+            modes.clear(); 
+            modes.push_back(node->val);
+        } else if (currCount == maxCount) {
+            modes.push_back(node->val);
+        }
+
+        prevNode = node;
+
+        inorder(node->right);
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        inorder(root);
+        return modes;
+    }
+};
