b425bd0d54f2b8e3d062d30b2a63f2ca30315614
itsnotTalha <talhajubayer737@gmail.com>
2025-07-24 22:11:20 +0600

    NeetCode 3

---
+++ NeetCode/TwoSums.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash;
+        vector<int>ans;
+        for(int i = 0; i<nums.size(); i++){
+            if(hash.count(target-nums[i])) {
+                ans.push_back(i);
+                ans.push_back(hash[target-nums[i]]);
+                break;
+            }else{
+                hash[nums[i]]=i;
+            }
+        }
+        return ans;
+    }
+};
