585f7fd34ac641166716b2911b9b8150c0ff6acf
itsnotTalha <talhajubayer737@gmail.com>
2025-08-04 08:20:17 +0600

    CF - DIV 3 - B

---
+++ B_No_Casino_in_the_Mountains.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef deque<int> di;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define pf push_front
+#define ppf pop_front
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define sz(x) int(x.size())
+
+#define for0(i, n) for (int i = 0; i < int(n); i++)
+#define for1(i, n) for (int i = 1; i <= int(n); i++)
+#define FOR(i, x, y) for (int i = int(x); i <= int(y); i++)
+#define ROF(i, x, y) for (int i = int(x); i >= int(y); i--)
+
+#define si(x) scanf("%d", &x)
+#define sii(x, y) scanf("%d %d", &x, &y)
+#define siii(x, y, z) scanf("%d %d %d", &x, &y, &z)
+#define sl(x) scanf("%lld", &x)
+#define sll(x, y) scanf("%lld %lld", &x, &y)
+#define slll(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
+
+#define ip(a, n) for0(i, n) cin >> a[i]
+
+int main() {
+    Fast_io;
+    int t; cin>>t;
+    while(t--){
+       int n,k;
+       cin>>n>>k;
+       vi a(n);
+       ip(a,n);
+       int ans = 0, cnt = 0; 
+       for0(i,n){
+            if(a[i]==1){
+                ans+=(cnt+1)/(k+1);
+                cnt = 0;
+                continue;
+            }else{
+                cnt++;
+            }
+        }
+        ans+=(cnt+1)/(k+1);
+        cout<<ans<<endl;
+    
+    }   
+    return 0;
+}
