f5525917f148e8b5da135c53b4d9056d9a472a77
itsnottalha <talhajubayer737@gmail.com>
2025-08-09 07:03:35 +0600

    NeetCode 150 - Trying

--- NeetCode/GroupAnagram.cpp
+++ NeetCode/GroupAnagram.cpp
@@ -1,8 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//       vector<pair<string,int>>tmp1;
+//       for(int i = 0; i<strs.size(); i++){
+//         tmp1.push_back(make_pair(strs[i], i));
+//       }
+//       sort(tmp1.begin(), tmp1.end());
+//       for(auto &a : tmp1){
+//         cout<<a.first<<" ";
+//       }
+//     }
+// };
+
+int main(){
+    vector<vector<string>> ans;
+    vector<string>strs;
+    for(int i = 0; i<6; i++){
+        string s;
+        cin>>s;
+        strs.push_back(s);
     }
-};
+    vector<pair<string,int>>tmp1;
+      for(int i = 0; i<strs.size(); i++){
+        tmp1.push_back(make_pair(strs[i], i));
+      }
+      sort(tmp1.begin(), tmp1.end());
+      for(auto &a : tmp1){
+        sort(a.first.begin(), a.first.end());
+      }
+      vector<string> prev;
+      for(auto &a : tmp1){
+        if(prev.empty()) prev.push_back(strs[a.second]);
+        else{
+            if(strs[a.second]==prev.back())prev.push_back(strs[a.second]);
+            else{
+                ans.push_back(prev);
+                prev.clear();
+                prev.push_back(strs[a.second]);
+            }
+        }
+      }
+      ans.push_back(prev);
+    return ans;
+}
