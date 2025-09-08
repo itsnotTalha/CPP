7ba0679daf32e6553a6909f8d0e91525c54c7507
itsnotTalha <talhajubayer737@gmail.com>
2025-09-08 21:55:18 +0600

    Parina

---
+++ A_Maple_and_Multiplication.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef deque<int> di;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define pf push_front
+#define ppf pop_front
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define sz(x) int(x.size())
+
+#define for0(i, n) for (int i = 0; i < int(n); i++)
+#define for1(i, n) for (int i = 1; i <= int(n); i++)
+#define FOR(i, x, y) for (int i = int(x); i <= int(y); i++)
+#define ROF(i, x, y) for (int i = int(x); i >= int(y); i--)
+
+#define si(x) scanf("%d", &x)
+#define sii(x, y) scanf("%d %d", &x, &y)
+#define siii(x, y, z) scanf("%d %d %d", &x, &y, &z)
+#define sl(x) scanf("%lld", &x)
+#define sll(x, y) scanf("%lld %lld", &x, &y)
+#define slll(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
+
+#define ip(a, n) for0(i, n) cin >> a[i]
+
+void solve(){
+    int a, b; cin>>a>>b;
+    if(a==b) cout<<0<<endl;
+    else if(a%b==0 or b%a==0) cout<<1<<endl;
+    else cout<<2<<endl;
+}
+
+int main() {
+    Fast_io;
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }   
+    return 0;
+}
