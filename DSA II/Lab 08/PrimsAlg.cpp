324bcd0fce47bcd0505e8142d59ed22a9f2fd95f
itsnotTalha <talhajubayer737@gmail.com>
2025-09-18 15:15:48 +0600

    DSA II Lab Mid

--- DSA II/Lab 08/PrimsAlg.cpp
+++ DSA II/Lab 08/PrimsAlg.cpp
@@ -1,2 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
+/*
+a -> b|2 -> d|2 -> c|1 -> NULL
+b -> a|2 -> d|6 -> c|4 -> NULL
+c -> a|1 -> d|10 -> c|1 -> NULL
+d -> c|10 -> a|2 -> b|6 -> e|6 -> NULL
+e -> b|4 -> d|6 -> c|1 -> NULL
+
+priority queue is needed -.-
+
+Properties: 
+key = u
+parent = V
+inMinST = False
+
+while(){
+is neighbor?
+outside MST?
+key larger?
+if yes:
+key[v] = wt
+pq.insert(key[v],v)
+parent[v]=u
+
+}
+*/
