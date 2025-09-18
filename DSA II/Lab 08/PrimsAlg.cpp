be632a466f1511ac49c65a2bd825a7904f19017e
itsnotTalha <talhajubayer737@gmail.com>
2025-09-18 16:26:07 +0600

    Prims Algorithm

--- DSA II/Lab 08/PrimsAlg.cpp
+++ DSA II/Lab 08/PrimsAlg.cpp
@@ -27,3 +27,115 @@ parent[v]=u
 
 }
 */
+
+class AdjList{
+public:
+int vertex, weight;
+AdjList(int vertex, int weight){
+    this->vertex = vertex;
+    this->weight = weight;
+}
+};
+
+int key[100], parent[100];
+bool inMST[100];
+
+struct Compare{
+    bool operator()(pair<int,int> &a, pair<int,int>&b){
+        return a.first>b.first;
+    }
+};
+
+void Prims(vector<vector<pair<int,int>>>  &adjL, int V, int E, int s){
+    for(int i = 1; i<=V; i++){
+        key[i] = INT_MAX;
+        parent[i] = -1;
+        inMST[i] = 0;
+    }
+
+    key[s] = 0;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq;
+
+    pq.push({s,0});
+
+    while(!pq.empty()){
+        pair<int,int> a = pq.top();
+        cout<<a.first<<" "<<a.second<<endl;
+        pq.pop();
+        int u = a.first;
+        if(inMST[u]) continue;
+
+        inMST[u]=true;
+
+        for(auto x : adjL[u]){
+            int v = x.first;
+            int w = x.second;
+            if(inMST[v]==false and key[v]>w){
+                key[v] = w;
+                parent[v] = u;
+                pq.push({v,key[v]});
+            }
+        }
+
+    }
+    cout<<"KEYSSS: ";
+    int sum = 0;
+    for(int i = 1; i<=V; i++){
+        sum+=key[i];
+        cout<<key[i]<<" ";
+    } 
+    cout<<endl;
+    cout<<"PARENTT: ";
+    for(int i = 1; i<=V; i++) cout<<parent[i]<<" ";
+    cout<<endl;
+    cout<<"inMST: ";
+    for(int i = 1; i<=V; i++) cout<<inMST[i]<<" ";
+    cout<<endl;
+    cout<<"Total weight : "<<sum<<endl;
+    /*
+        selected edges ber korte hobe
+        1, 2, 4
+        8, 2, 5
+        2, 3, 9
+        ...
+    */
+}
+
+int main(){
+    cout<<"Enter the number of vertices and edges "<<endl;
+    int V, E;
+    cin>>V>>E;
+
+    vector<vector<pair<int,int>>>  adjL(V+1);
+
+    cout<<"Enter the edges: "<<endl;
+    for(int i = 1; i<=E; i++){
+        //s is start, e is end and w is weight of the edge
+        int s, e, w; cin>>s>>e>>w;
+        adjL[s].push_back({e,w});
+    }
+    int i = 0;
+    for(auto x : adjL){
+        cout<<i<<" -> ";
+        for(auto s : x){
+            cout<<s.first<<"|"<<s.second<<" -> ";
+        }
+        cout<<"NULL"<<endl;
+        i++;
+    }
+    Prims(adjL, V, E, 1);
+}
+
+/*
+8 10
+1 2 4
+1 8 6
+2 3 9
+2 8 5
+2 5 2
+5 4 15
+5 6 8
+6 7 3
+7 8 14
+6 8 10
+*/
