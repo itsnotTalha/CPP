2b6e96a1ef3f80e327bc8be8d1a54b9b3764ecd4
itsnotTalha <talhajubayer737@gmail.com>
2025-09-23 13:48:53 +0600

    Prims Algorithm Practice

--- DSA II/Practice/Prims.cpp
+++ DSA II/Practice/Prims.cpp
@@ -133,3 +133,53 @@ class PrimsAlgo{
     }
 
 };
+
+class AnotherPrim{
+  int key[100], Parent[100];
+  bool inMst[100];
+  
+  struct Compare{
+    bool operator()(pair<int,int> &a, pair<int,int> &b){
+        return a.first>b.first;
+    }
+  };
+
+  void Prims(vector<vector<pair<int,int>>> &AdjL, int v, int e, int s){
+    for(int i = 1; i<=v; i++){
+        key[i] = INT_MAX;
+        Parent[i] = -1;
+        inMst[i] = false;
+    }
+    key[s] = 0;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq;
+    pq.push({s,0});
+    while(!pq.empty()){
+        pair<int, int> a = pq.top();
+        int u = a.first;
+        pq.pop();
+        if(inMst[u]) continue;
+        inMST[u] = true;
+        for(auto x : AdjL[u]){
+            int v = x.first;
+            int w = x.second;
+            if(inMST[v]==false and key[v]>w){
+                key[v] = w;
+                Parent[v] = u;
+                pq.push({v,w});
+            }
+        }
+    }
+  }
+
+  int main(){
+    cout<<"Enter vertices and edges: ";
+    int V, E; cin>>V>>E;
+    vector<vector<pair<int,int>>> AdjL(V+1);
+    for(int i = 1; i<=V; i++){
+        int s, e, w;
+        cin>>s>>e>>w;
+        AdjL[s].push_back({e,w});
+    }
+    Prims(AdjL, V, E, 1);
+  }
+};
