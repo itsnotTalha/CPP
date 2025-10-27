954ab5a0cd8c56730d023691922de0e0e401a5e4
itsnotTalha <talhajubayer737@gmail.com>
2025-10-27 11:42:47 +0600

    Custom Hash func

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,10 @@
-
-int main() {
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m;
+    cin>>n;
+    // cout<<n%m<<endl;
+    cout<<"Hash 1: ";
+    cout<<(2*n+7)%13<<endl;
+    cout<<"Hash 2: "<<(3*n %13)+1<<endl;
+}
